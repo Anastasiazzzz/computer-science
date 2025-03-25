7d6affda1b531062b8ef9afbb37b7377b0176471
Anastasiazzzz <stce.fr@gmail.com>
2025-03-25 22:02:52 +0500

    new test

--- test_main.py
+++ test_main.py
@@ -1,2 +1,7 @@
-def test_example():
-    assert True
+def add(a, b):
+    return a + b
+
+def test_addition():
+    result = add(2, 3)
+    expected_result = 5
+    assert result == expected_result, f"Ожидалось {expected_result}, получилось {result}"
